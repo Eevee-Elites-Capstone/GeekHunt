2b7128c7cde64a696e72fd0a440d95daae1cdaf9
speedturtle28 <jackw4@vt.edu>
2022-01-31 21:04:22 +0000

    Added Skills, githubUrl, and linkedinURL fields

--- src/components/Auth/GeekForm.js
+++ src/components/Auth/GeekForm.js
@@ -41,24 +41,78 @@ function GeekForm () {
             />
           </div>
           
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-normal mb-2"
+              htmlFor="skills"
+            >
+              Skills
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              name="skills"
+              v-model="form.skills"
+              type="skills"
+              required
+              autoFocus
+              placeholder="Skills"
+              onChange={(e) => setSkills(e.target.value)}
+              value={skills}
+            />
+          </div>
+          
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-normal mb-2"
+              htmlFor="linkedInUrl"
+            >
+              LinkedIn Url
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              name="linkedInUrl"
+              v-model="form.linkedInUrl"
+              type="linkedInUrl"
+              required
+              autoFocus
+              placeholder="LinkedInUrl"
+              onChange={(e) => setLinkedInUrl(e.target.value)}
+              value={linkedInUrl}
+            />
+          </div>
+          
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-normal mb-2"
+              htmlFor="gitHubUrl"
+            >
+              GitHub Url
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              name="gitHubUrl"
+              v-model="form.gitHubUrl"
+              type="gitHubUrl"
+              required
+              autoFocus
+              placeholder="GitHubUrl"
+              onChange={(e) => setGitHubUrl(e.target.value)}
+              value={gitHubUrl}
+            />
+          </div>
+          
           <div className="flex items-center justify-between">
             {!isPending && <button
               className="px-4 py-2 rounded text-white inline-block shadow-lg bg-blue-500 hover:bg-blue-600 focus:bg-blue-700"
               type="submit"
             >
-              Sign Up
+              Update Geek Profile
             </button>}
             {isPending && <button
               className="px-4 py-2 rounded text-white inline-block shadow-lg bg-blue-500 hover:bg-blue-600 focus:bg-blue-700"
               disabled
             >Loading...</button>}
             {error && <p>{error}</p>}
-            <p
-              className="inline-block align-baseline font-normal text-sm text-blue-500 hover:text-blue-800"
-              href="#"
-            >
-              Sign Up with Google
-            </p>
           </div>
           </form>
          </div>
