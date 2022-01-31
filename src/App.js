e4f8599d319fbc5ecdb867bb4fc6166cc42335bf
Melissa Molina <melissa.mm.itt@gmail.com>
2022-01-31 17:54:48 -0500

    geek or hunter

--- src/App.js
+++ src/App.js
@@ -2,9 +2,7 @@ import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import './App.css';
 import SignIn from './components/Auth/SignIn';
 import SignUp from './components/Auth/SignUp';
-import GeekForm from './components/Auth/GeekForm';
 import Home from './components/Home';
-import HunterForm from './components/Auth/HunterForm';
 import Navbar from './components/UI/Navbar';
 import Footer from './components/UI/Footer';
 import { useAuthContext } from './hooks/useAuthContext';
@@ -15,7 +13,7 @@ function App() {
   return (
     <div className="App">
       <div>
-   git 
+      <Navbar />
       </div>
       <div>
       {authIsReady && (
@@ -28,14 +26,8 @@ function App() {
             <Route path="/signin">
               <SignIn />
             </Route>
-            <Route exact path="/signup">
+            <Route path="/signup">
               {user ? <Redirect to="/"/> : <SignUp />}
-            </Route>
-            <Route path="/hunter">
-              {HunterForm}
-            </Route>
-            <Route exact path="/signup/geek">
-              {user ? <Redirect to="/"/> : <GeekForm />}
             </Route>
           </Switch>
         </BrowserRouter>
